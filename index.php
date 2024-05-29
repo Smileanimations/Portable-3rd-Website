40c031dcaac4baafa2de1d781f033c789938e3da
Mark <85407678+Smileanimations@users.noreply.github.com>
2024-05-29 23:10:34 +0200

    fewweqr

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-lg-3 rounded-3 border p-2 mx-4" id="P3rd">
+                <a href="Portable 3rd/index.php" id="P3rdlogo"><img src="Portable 3rd/images/logo/logo-MHP3.png" alt="" width="159px" class="logo"></a>
+            </div>
+        
+            <div class="col-lg-3 rounded-3 border p-2 mx-4" id="Dos">
+                <img src="Dos/images/logo-MH2.webp" alt="" width="140px" class="logo" >
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
